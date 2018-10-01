5a1cd2b0987605d6660585a355638f174e664d0d
udugam <ryan.udugam@gmail.com>
2018-10-01 12:56:37 -0400

    added comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,16 @@
 var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema;
+
+var CommentSchema = new Schema ({
+    content: {
+        type: String,
+        required: true
+    }
+})
+
+//Create collection "articles" based on above Schema
+var Comment = mongoose.model("Comment",CommentSchema)
+
+//Export reference to articles collection
+module.exports = Comment
